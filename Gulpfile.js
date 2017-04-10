887da252a30f3428666bba5e519a1be97270c9b5
Noah Walker <noahwalker3509@gmail.com>
2017-04-09 22:09:14 -0600

    Remove server and interactive table, remove node, simplify

--- Gulpfile.js
+++
@@ -1,9 +0,0 @@
-//Run 'gulp' on the command line to start the server.
-var gulp = require('gulp'),
-  connect = require('gulp-connect');
- 
-gulp.task('connect', function() {
-  connect.server();
-});
- 
-gulp.task('default', ['connect']);
---
+++ README.md
@@ -0,0 +1,3 @@
+# ECS Boilerplate - Collection of HTML5 Boilerplates for Exploring Computer Science Class
+
+Small class project for high school to make the class easier to do.
